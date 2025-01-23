ca86ed70eba9a1fedbb8e9d7c49cae809c3bc896
mkhurziya11 <khurziya.maratova@rectorywolves.org>
2025-01-23 07:01:05 -1000

    Mrs. Karp Commit comment

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,6 @@
 pt = input("Input a plaintext: ").upper()
 k = input("Input a key: ").upper()
+# Help from Mrs. Karp
 
 alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
