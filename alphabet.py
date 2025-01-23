b5cbf9601b65aba05f8c2225d83fe9f9165aa63b
mkhurziya11 <khurziya.maratova@rectorywolves.org>
2025-01-23 06:48:51 -1000

    completed code2

--- alphabet.py
+++ alphabet.py
@@ -1,25 +1,24 @@
-def A(m, N):
-	alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-	d_m = []
-	for c in m:
-		x = alph.index(c) + N
-		if x > 25:
-			x -= 26
-		d_m.append(alph[x])
-	return d_m
+pt = input("Input a plaintext: ").upper()
+k = input("Input a key: ").upper()
 
-def cypher(N):
-	alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-	l = {}
-	for i in range(len(alph)):
-		x = i + N
-		if x > (len(alph) -1):
-			x = x - len(alph)
-		l[alph[i]] = alph[x]
-	return l
+alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+KeyStream = []
+encodedPhrase = []
+
+def keyStreamCreator(k, pt):
+	j = 0
 	
-m = input("Input your message: ").upper()
-shift = int(input("Shift number: "))
+	for i in range(len(pt)):
+		j = i
+		
+		while j >= len(k):
+			j = j - len(k)
+		
+		keyStream.append(k[j])
+	
+	print(keyStream)
 
-a = A(m, shift)
-print("Encoded Message:", ''.join(a))
+keyStreamCreator(k, pt)
+
+
